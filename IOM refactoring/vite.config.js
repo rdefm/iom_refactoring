c12f9c1f51da5bac23633571995cb04652d32a4e
rdefm <r.defazio.maddocks@gmail.com>
2026-06-21 13:00:23 +0000

    Fix vite config

--- IOM refactoring/vite.config.js
+++ IOM refactoring/vite.config.js
@@ -25,4 +25,15 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 }
+            }
+          }
+        ]
+      }
